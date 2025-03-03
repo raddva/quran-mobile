ac3b665a797a4aace894cff1fc33109f23e2e6b1
raddva <111165508+raddva@users.noreply.github.com>
2025-03-03 14:03:49 +0700

    fix profile ui

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:quran_mobile/auth/auth_service.dart';
 import 'package:quran_mobile/auth/forgot_password_screen.dart';
-import 'package:quran_mobile/auth/signin_screen.dart';
 import 'package:quran_mobile/widgets/button.dart';
 import 'package:quran_mobile/widgets/textfield.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:quran_mobile/widgets/alert_dialog.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -33,90 +33,113 @@ class _SettingsScreenState extends State<SettingsScreen> {
   }
 
   void updateUserInfo() async {
-    String newName = nameController.text;
-    String newEmail = emailController.text;
+    showCustomAlertDialog(
+      context,
+      "Confirm Deletion",
+      "Are you sure you want to delete this note?",
+      onConfirm: () async {
+        String newName = nameController.text;
+        String newEmail = emailController.text;
 
-    bool success = await auth.updateUserInfo(newName, newEmail);
+        bool success = await auth.updateUserInfo(newName, newEmail);
 
-    if (success) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('User information updated successfully!')),
-      );
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to update user information.')),
-      );
-    }
+        if (success) {
+          showSuccessAlert(context, "User information updated successfully.");
+        } else {
+          showCustomAlertDialog(
+              context, "Failed", "Failed to update user information.");
+        }
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                SizedBox(height: 20),
-                Icon(
+      backgroundColor: Colors.white,
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            floating: false,
+            expandedHeight: 50,
+            centerTitle: true,
+            backgroundColor: Colors.transparent,
+            title: Text(
+              "Profile",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.green[700],
+                fontSize: 16,
+              ),
+            ),
+            actions: [
+              IconButton(
+                icon: Icon(CupertinoIcons.square_arrow_left, color: Colors.red),
+                onPressed: () async {
+                  await auth.signOut();
+                  Get.offNamed('/auth');
+                },
+                tooltip: "Sign Out",
+              ),
+            ],
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              const SizedBox(height: 20),
+              Center(
+                child: Icon(
                   CupertinoIcons.profile_circled,
                   color: Colors.green,
-                  size: 250,
+                  size: 150,
                 ),
-                SizedBox(height: 40),
-                CustomTextField(
-                  controller: nameController,
-                  hint: "Display Name",
-                  isDisabled: true,
-                  label: "Name",
-                  icon: CupertinoIcons.person,
-                ),
-                SizedBox(height: 20),
-                CustomTextField(
-                  controller: emailController,
-                  hint: "Email",
-                  isDisabled: true,
-                  label: "Email",
-                  icon: CupertinoIcons.mail,
-                ),
-                SizedBox(height: 20),
-                // CustomButton(
-                //   label: "Save Changes",
-                //   onPressed: updateUserInfo,
-                // ),
-                SizedBox(height: 50),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ForgotPasswordScreen(),
-                        ));
-                  },
-                  child: Text(
-                    "Change Password",
-                    style: TextStyle(
-                      color: Colors.green[600],
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    CustomTextField(
+                      controller: nameController,
+                      hint: "Display Name",
+                      // isDisabled: true,
+                      label: "Name",
+                      icon: CupertinoIcons.person,
                     ),
-                  ),
+                    const SizedBox(height: 20),
+                    CustomTextField(
+                      controller: emailController,
+                      hint: "Email",
+                      // isDisabled: true,
+                      label: "Email",
+                      icon: CupertinoIcons.mail,
+                    ),
+                    const SizedBox(height: 20),
+                    TextButton(
+                      onPressed: () {
+                        Get.to(() => const ForgotPasswordScreen());
+                      },
+                      child: Text(
+                        "Change Password",
+                        style: TextStyle(
+                          color: Colors.green[600],
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    CustomButton(
+                      label: "Save Changes",
+                      onPressed: updateUserInfo,
+                    ),
+                    const SizedBox(height: 50),
+                  ],
                 ),
-                SizedBox(height: 40),
-                CustomButton(
-                  label: "Sign Out",
-                  onPressed: () async {
-                    await auth.signOut();
-                    // goToLogin(context);
-                    Get.offNamed('/auth');
-                  },
-                )
-              ],
-            ),
+              ),
+            ]),
           ),
-        ));
+        ],
+      ),
+    );
   }
 }
-
-goToLogin(BuildContext context) => Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const LoginScreen()),
-    );
